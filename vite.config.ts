fb1b550a02796acd2815cb6d3ea249dabdefb9f3
Alicia Sykes <alicia@omg.lol>
2025-05-25 13:56:37 +0100

    Updates vite config to support static mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,22 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import preprocess from 'svelte-preprocess';
+import adapterStatic from '@sveltejs/adapter-static';
+import adapterAuto from '@sveltejs/adapter-auto';
 
-export default defineConfig({
-	plugins: [sveltekit()]
-});
+const isStatic = process.env.IS_STATIC === 'true' || false;
+const base = process.env.BASE_PATH ?? '';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: preprocess(),
+	plugins: [sveltekit()],
+	kit: {
+		adapter: isStatic ? adapterStatic() : adapterAuto(),
+		paths: {
+			base,
+		}
+	}
+};
+
+export default defineConfig(config);
