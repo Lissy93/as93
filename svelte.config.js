40987975d59aa0f03a010f78fc8200fa3dcfc1b7
Alicia Sykes <alicia@omg.lol>
2025-05-25 15:14:38 +0100

    Update svelte config, rever vite config

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,20 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { sveltekit } from '@sveltejs/kit/vite';
+import preprocess from 'svelte-preprocess';
+import adapterStatic from '@sveltejs/adapter-static';
+import adapterAuto from '@sveltejs/adapter-auto';
+
+const isStatic = process.env.IS_STATIC === 'true' || false;
+const base = process.env.BASE_PATH ?? '';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
+	preprocess: preprocess(),
+	plugins: [sveltekit()],
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: isStatic ? adapterStatic() : adapterAuto(),
+		paths: {
+			base,
+		}
 	}
 };
 
