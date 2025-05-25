dc683d12c1606495b3eed1921464b8e4df981676
Alicia Sykes <alicia@omg.lol>
2025-05-25 15:26:37 +0100

    Fix deployment hanging

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,15 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import preprocess from 'svelte-preprocess';
-import adapterStatic from '@sveltejs/adapter-static';
 import adapterAuto from '@sveltejs/adapter-auto';
+import adapterStatic from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+// import { sveltekit } from '@sveltejs/kit/vite';
 
 const isStatic = process.env.IS_STATIC === 'true' || false;
 const base = process.env.BASE_PATH ?? '';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: preprocess(),
-	plugins: [sveltekit()],
+	preprocess: vitePreprocess(),
+	// plugins: [sveltekit()],
 	kit: {
 		adapter: isStatic ? adapterStatic() : adapterAuto(),
 		paths: {
--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,29 @@
-// import { sveltekit } from '@sveltejs/kit/vite';
-// import { defineConfig } from 'vite';
-
-// export default defineConfig({
-// 	plugins: [sveltekit()]
-// });
-
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import preprocess from 'svelte-preprocess';
-import adapterStatic from '@sveltejs/adapter-static';
-import adapterAuto from '@sveltejs/adapter-auto';
 
-const isStatic = process.env.IS_STATIC === 'true' || false;
-const base = process.env.BASE_PATH ?? '';
+export default defineConfig({
+	plugins: [sveltekit()]
+});
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	preprocess: preprocess(),
-	plugins: [sveltekit()],
-	kit: {
-		adapter: isStatic ? adapterStatic() : adapterAuto(),
-		paths: {
-			base,
-		}
-	}
-};
+// import { sveltekit } from '@sveltejs/kit/vite';
+// import { defineConfig } from 'vite';
+// import preprocess from 'svelte-preprocess';
+// import adapterStatic from '@sveltejs/adapter-static';
+// import adapterAuto from '@sveltejs/adapter-auto';
 
-export default defineConfig(config);
+// const isStatic = process.env.IS_STATIC === 'true' || false;
+// const base = process.env.BASE_PATH ?? '';
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	preprocess: preprocess(),
+// 	plugins: [sveltekit()],
+// 	kit: {
+// 		adapter: isStatic ? adapterStatic() : adapterAuto(),
+// 		paths: {
+// 			base,
+// 		}
+// 	}
+// };
+
+// export default defineConfig(config);
