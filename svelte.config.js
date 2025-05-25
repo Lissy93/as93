44b3c1ec32afdb71e996d08de87b76505f99a824
Alicia Sykes <alicia@omg.lol>
2025-05-25 22:15:34 +0100

    Fetch repos to pre-render

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,41 @@
 import adapterAuto from '@sveltejs/adapter-auto';
 import adapterStatic from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-// import { sveltekit } from '@sveltejs/kit/vite';
 
 const isStatic = process.env.IS_STATIC === 'true' || false;
 const base = process.env.BASE_PATH ?? '';
+const githubToken = process.env.GITHUB_TOKEN;
+
+async function getGithubSlugs() {
+	if (!githubToken) return [];
+	try {
+		const res = await fetch(`https://api.github.com/users/lissy93/repos?per_page=100`, {
+			headers: { Authorization: `Bearer ${githubToken}` }
+		});
+		if (!res.ok) throw new Error(`GitHub API failed: ${res.status}`);
+		const repos = await res.json();
+		return repos.map((repo) => `/${repo.name}`);
+	} catch (err) {
+		console.warn(`⚠️ Failed to fetch GitHub repos: ${err.message}`);
+		return [];
+	}
+}
+
+const githubSlugs = await getGithubSlugs();
+
+console.log('Slugs: ', githubSlugs);
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
-	// plugins: [sveltekit()],
 	kit: {
 		adapter: isStatic ? adapterStatic() : adapterAuto(),
 		paths: {
 			base,
+		},
+		prerender: {
+			entries: githubSlugs,
 		}
 	}
 };
